5ffcb096bd9bd4f3ffe9e6787ea896c6f3b8065b
Limoos <54327152+Lim0os@users.noreply.github.com>
2025-09-09 21:46:38 +0300

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -29,8 +29,166 @@ const default_config = {
   openLastVisitedTab: true,
   tabs: [
     {
+      name: "chi ll",
+      background_url: "src/img/banners/cbg-2.gif",
+      categories: [{
+        name: "Social Media",
+        links: [
+          {
+            name: "whatsapp",
+            url: "https://web.whatsapp.com/",
+            icon: "brand-whatsapp",
+            icon_color: "#a9b665",
+          },
+          {
+            name: "twitter",
+            url: "https://twitter.com/home",
+            icon: "brand-twitter-filled",
+            icon_color: "#7daea3",
+          },
+          {
+            name: "reddit",
+            url: "https://www.reddit.com/",
+            icon: "brand-reddit",
+            icon_color: "#e78a4e",
+          },
+          {
+            name: "youtube",
+            url: "https://www.youtube.com/",
+            icon: "brand-youtube-filled",
+            icon_color: "#ea6962",
+          },
+          {
+            name: "twitch",
+            url: "https://www.twitch.tv/",
+            icon: "brand-twitch",
+            icon_color: "#d3869b",
+          },
+        ],
+      }, {
+        name: "Games",
+        links: [
+          {
+            name: "chess",
+            url: "https://www.chess.com/home",
+            icon: "chess-queen-filled",
+            icon_color: "#a9b665",
+          },
+          {
+            name: "monkeytype",
+            url: "https://monkeytype.com/",
+            icon: "keyboard",
+            icon_color: "#e78a4e",
+          },
+          {
+            name: "tetris",
+            url: "https://tetris.com/",
+            icon: "brand-apple-arcade",
+            icon_color: "#ea6962",
+          },
+        ],
+      }, {
+        name: "Video",
+        links: [
+          {
+            name: "disney+",
+            url: "https://www.disneyplus.com/home",
+            icon: "brand-disney",
+            icon_color: "#7daea3",
+          },
+          {
+            name: "primevideo",
+            url: "https://www.primevideo.com/region/eu/?ref_=dv_web_unknown",
+            icon: "brand-amazon",
+            icon_color: "#7daea3",
+          },
+        ],
+      }],
+    },
+    {
+      name: "design",
+      background_url: "src/img/banners/cbg-6.gif",
+      categories: [
+        {
+          name: "inspiration",
+          links: [
+            {
+              name: "pinterest",
+              url: "https://www.pinterest.es/",
+              icon: "brand-pinterest",
+              icon_color: "#ea6962",
+            },
+            {
+              name: "artstation",
+              url: "https://www.artstation.com/?sort_by=community",
+              icon: "chart-area",
+              icon_color: "#7daea3",
+            },
+            {
+              name: "leonardo ai",
+              url: "https://app.leonardo.ai/",
+              icon: "brand-openai",
+              icon_color: "#89b482",
+            },
+            {
+              name: "dribble",
+              url: "https://dribbble.com/following",
+              icon: "brand-dribbble-filled",
+              icon_color: "#d3869b",
+            },
+          ],
+        },
+        {
+          name: "resources",
+          links: [
+            {
+              name: "figma",
+              url: "https://www.figma.com",
+              icon: "brand-figma",
+              icon_color: "#d3869b",
+            },
+            {
+              name: "uxpro",
+              url: "https://uxpro.cc/",
+              icon: "components",
+              icon_color: "#a9b665",
+            },
+            {
+              name: "colorhunt",
+              url: "https://colorhunt.co/",
+              icon: "color-picker",
+              icon_color: "#ea6962",
+            },
+            {
+              name: "adobe color",
+              url: "https://color.adobe.com/es/create/color-wheel",
+              icon: "brand-adobe",
+              icon_color: "#7daea3",
+            },
+            {
+              name: "terminalsexy",
+              url: "https://terminal.sexy",
+              icon: "prompt",
+              icon_color: "#e78a4e",
+            },
+          ],
+        },
+        {
+          name: "resources 3d",
+          links: [
+            {
+              name: "thingiverse",
+              url: "https://www.thingiverse.com/",
+              icon: "circle-letter-t",
+              icon_color: "#7daea3",
+            },
+          ],
+        },
+      ],
+    },
+    {
       name: "dev",
-      background_url: "src/img/banners/cbg-8.gif",
+      background_url: "src/img/banners/cbg-7.gif",
       categories: [
         {
           name: "repositories",
@@ -42,21 +200,10 @@ const default_config = {
               icon_color: "#7daea3",
             },
             {
-              name: "terra gitlab",
-              url: "https://gitlab.m-2-m.online/",
+              name: "gitlab",
+              url: "https://gitlab.com/",
               icon: "brand-gitlab",
               icon_color: "#e78a4e",
-            },
-          ],
-        },
-        {
-          name: "work sites",
-          links: [
-            {
-              name: "terra",
-              url: "https://devterra.m-2-m.online/",
-              icon: "biohazard",
-              icon_color: "#ea6962",
             },
           ],
         },
@@ -64,10 +211,28 @@ const default_config = {
           name: "resources",
           links: [
             {
+              name: "phind",
+              url: "https://www.phind.com/",
+              icon: "brand-openai",
+              icon_color: "#89b482",
+            },
+            {
+              name: "flutter",
+              url: "https://docs.flutter.dev/ui",
+              icon: "brand-flutter",
+              icon_color: "#7daea3",
+            },
+            {
               name: "hacktricks",
               url: "https://book.hacktricks.xyz/welcome/readme",
               icon: "biohazard",
               icon_color: "#ea6962",
+            },
+            {
+              name: "vscode",
+              url: "https://vscode.dev/",
+              icon: "brand-vscode",
+              icon_color: "#7daea3",
             },
           ],
         },
@@ -92,6 +257,12 @@ const default_config = {
               icon: "brand-onedrive",
               icon_color: "#ea6962",
             },
+            {
+              name: "hackerrank",
+              url: "https://www.hackerrank.com/dashboard",
+              icon: "code-asterix",
+              icon_color: "#a9b665",
+            },
           ],
         },
       ],
@@ -112,46 +283,28 @@ const default_config = {
           ],
         },
         {
-          name: "social",
+          name: "storage",
           links: [
             {
-              name: "telegram",
-              url: "https://web.telegram.org/k/",
-              icon: "brand-telegram",
+              name: "drive",
+              url: "https://drive.google.com/drive/u/0/my-drive",
+              icon: "brand-google-drive",
+              icon_color: "#e78a4e",
+            },
+            {
+              name: "dropbox",
+              url: "https://www.dropbox.com/h?role=personal&di=left_nav",
+              icon: "box-seam",
               icon_color: "#7daea3",
             },
             {
-              name: "vk",
-              url: "https://vk.com/feed",
-              icon: "brand-vk",
-              icon_color: "#7daea3",
+              name: "fotos",
+              url: "https://photos.google.com/u/1",
+              icon: "photo-filled",
+              icon_color: "#ea6962",
             },
           ],
         },
-        {
-          name: "funny",
-          links: [
-            {
-              name: "youtube",
-              url: "https://www.youtube.com/",
-              icon: "brand-youtube",
-              icon_color: "#7daea3",
-            },
-            {
-              name: "кинопоиск",
-              url: "https://hd.kinopoisk.ru/",
-              icon: "brand-kinopoisk",
-              icon_color: "#7daea3",
-            },
-            {
-              name: "музыка",
-              url: "https://music.yandex.ru/",
-              icon: "brand-spotify",
-              icon_color: "#7daea3",
-            }
-          ],
-        },
-
         {
           name: "stuff",
           links: [
